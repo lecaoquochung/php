76430fad1ce50ed1b56cde9172b2594b8c35272b
hungle <lecaoquochung@gmail.com>
2016-12-12 12:10:50 +0900

    [sendmail] sendmail test code

---
+++ sendmail/index.php
@@ -0,0 +1,44 @@
+<?php
+echo 'TEST SENDMAIL SERVER: '.$_SERVER['HTTP_HOST'];
+echo '<br />';
+echo $_SERVER['HTTP_HOST'] !== 'sem-check.givery.co.jp'?$test_account:'';
+$sender_ip = $_SERVER['REMOTE_ADDR'];
+$admin = "le.hung@givery.co.jp" ;
+if (isset($_REQUEST['email']) && $_REQUEST['email']!="")
+//if "email" is filled out, send email
+  {
+  //send email
+  $email = $_REQUEST['email'] ;
+  $subject = $_REQUEST['subject'] ;
+  $message = $_REQUEST['message'] ."\r\n"
+                .'This email was sent from IP:'.$sender_ip ."\r\n"
+                .'Please, contact admin if something went wrong '.$admin ;
+  $from = $_REQUEST['from'] ;
+
+  $headers = 'From: '.$_REQUEST['from'] . "\r\n" .
+    'Reply-To: ' .$_REQUEST['from'] . "\r\n" .
+    'Bcc: ' .$admin . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+  mail($email, $subject, $message, $headers);
+  echo "Mail sent";
+  echo "<br />";
+  echo 'Your IP will be saved: '.$sender_ip;
+  }
+else
+//if "email" is not filled out, display the form
+  {
+#        echo "From:".$from;
+#        echo "<br />";
+         echo "
+        <form method='post' action='sendmail.php'>
+        From: <input name='from' type='text' value='".$admin."'><br />
+        To Email: <input name='email' type='text'><br />
+        Subject: <input name='subject' type='text'><br />
+        Message:<br>
+        <textarea name='message' rows='15' cols='40'>
+        </textarea><br>
+        <input type='submit'>
+        </form>";
+  }
+?>
