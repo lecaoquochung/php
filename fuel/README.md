357f9d4ac6287ba2a5ccebf02248662177a93912
hungle <lecaoquochung@gmail.com>
2017-02-09 15:16:52 +0900

    Oil scaffold table users

--- fuel/README.md
+++ fuel/README.md
@@ -5,4 +5,7 @@
 ```
 # 01 users
 php oil g scaffold users username:string[200] password:string[255] group:integer[11] email:string[255] last_login:string[25] login_hash:string[255] profile_fields:text name:text
+
+# Migrate to lastest database
+php oil refine migrate
 ```
--- fuel/fuel/app/config/development/db.php
+++ fuel/fuel/app/config/development/db.php
@@ -6,9 +6,9 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev',
-			'username'   => 'root',
-			'password'   => 'root',
+			'dsn'        => 'mysql:host=mysql.local;dbname=docker',
+			'username'   => 'docker',
+			'password'   => 'docker',
 		),
 	),
 );
